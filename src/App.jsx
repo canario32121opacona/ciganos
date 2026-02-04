7dd2039f838e46abd0c36f835ef217cc60d094c9
Danish Rasheed <danirasheed60.com>
2026-02-04 11:45:22 +0000

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouting from "./AppRouting.jsx";
 import Navbar from "./components/navbar/navbar.jsx";
+import Footer from "./components/footer/footer.jsx";
 function App() {
 
   return (
   <BrowserRouter>
   <Navbar/>
     <AppRouting/>
+    <Footer/>
   </BrowserRouter>    
   );
 }
